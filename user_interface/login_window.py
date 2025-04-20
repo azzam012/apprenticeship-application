ae1c429fa584e0e4cce427c2fb99de58cdf900d7
azzam012 <azzoomm31@gmail.com>
2025-04-20 17:53:37 +0300

    aa

--- user_interface/login_window.py
+++ user_interface/login_window.py
@@ -1,33 +1,49 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QLineEdit
 from PyQt5.uic import loadUi
+from student_signup import StudentSignup
+#from company_signup import CompanySignupDialog
+
+
 
 class LoginDialog(QDialog):
     def __init__(self):
         super().__init__()
         loadUi("user_interface/login.ui", self)  # the location of login window in Qt Designer
-        self.setFixedSize(self.size())
+        #self.setFixedSize(self.size())
+        self.showMaximized()
 
-        self.student_login_button.clicked.connect(self.login_as_student)
-        self.company_login_button.clicked.connect(self.login_as_company)
+        self.login_button.clicked.connect(self.handle_login)
+
+        self.student_signup_button.clicked.connect(self.open_student_signup)
+        #self.company_signup_button.clicked.connect(self.open_company_signup)
         self.password_input.setEchoMode(QLineEdit.Password) # to make the password hidden
     
-    def login_as_student(self): # method 1: if the user is student
-        self.handle_login('student')
-        print('student')
-    
-    def login_as_company(self): # method 2: if the user is company
-        self.handle_login('company')
-        print('company')
 
-    def handle_login(self , user_typ): # method 3: to save info in the db
+    def handle_login(self): # method 3: to save info in the db
         email = self.email_input.text()
         password = self.password_input.text()
-        print(email,password)
+        
         
         #confirming that the user does not forgot to type in the email and password
         if not email or not password:
             QMessageBox.warning(self,"Error!"," you may forgit to type in the email ro the password")
+            return
+        
+        if self.student_radio.isChecked():
+            print('Logging in as student')
+        elif self.company_radio.isChecked():
+            print('Logging in as company')
+        else:
+            QMessageBox.warning(self,'Error!','Please select your user type.')
+
+    def open_student_signup(self):
+        self.student_signup_window = StudentSignup()
+        self.student_signup_window.exec_()
+
+    #def open_company_signup(self):
+        #self.company_signup_window = CompanySignupDialog()
+       # self.company_signup_window.exec_()
 
 
 
---
+++ user_interface/student_signup.py
@@ -0,0 +1,51 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox, QLineEdit
+from PyQt5.uic import loadUi
+from models.student import add_student  # تأكد أن مسار الاستيراد صحيح
+
+class StudentSignup(QDialog):
+    def __init__(self):
+        super().__init__()
+        loadUi("user_interface/student_signup.ui", self)
+        self.showFullScreen()
+
+        self.signup_button.clicked.connect(self.register_student)
+        self.password_input.setEchoMode(QLineEdit.Password) # to make the password hidden
+
+    def register_student(self):
+        name = self.name_input.text()
+        student_id = self.id_input.text()
+        email = self.email_input.text()
+        password = self.password_input.text()
+        gpa = self.gpa_input.text()
+        mobile = self.mobile_input.text()
+        specialization = self.specialization_input.text()
+        skills = self.skills_input.text()
+        locations = self.locations_input.text()
+
+        # تحقق من الحقول الأساسية
+        if not all([name, student_id, email, password, gpa, mobile, specialization, skills, locations]):
+            QMessageBox.warning(self, "Error", "Please fill in all fields")
+            return
+
+        try:
+            gpa = float(gpa)
+        except ValueError:
+            QMessageBox.warning(self, "Error", "GPA must be a number")
+            return
+
+        try:
+            add_student(student_id, name, mobile, email, gpa, specialization, locations, skills)
+            QMessageBox.information(self, "Success", "Student registered successfully")
+            self.close()
+        except Exception as e:
+            QMessageBox.critical(self, "Database Error", str(e))
+
+# للاختبار فقط
+        if name == "__main__":
+            app = QApplication(sys.argv)
+            window = StudentSignup()
+            window.show()
+            sys.exit(app.exec_())
